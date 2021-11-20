12edb527d776de11516b21fc926274b5dc0d55a9
Ian Marsman <ian.marsman@gmail.com>
2021-11-20 12:55:05 -0500

    Cleaned and tested invocation script

--- build/assets/run.sh
+++ build/assets/run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "running"
+# This script takes an argument to docker run and turns it into a complete call
+# invocation for ops. If this is not done then sub-parts of the call like
+# "image list" in "ops image list" will not be handled properly. Thus this
+# script makes a proper invocation and then uses it to call ops.
 
-to_run="/app/ops $@"
-echo "running $to_run"
+# Get args to docker run call as a single string
+args="$*"
 
+# Remove any leading ops invocaton using greedy
+args="${args#*ops }"
 
-eval $to_run
+# Create the call, adding back ops invocation
+to_run="/app/ops $args"
+
+echo $to_run
+
+# Run created call
+eval "$to_run"
